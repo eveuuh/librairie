beed8e93d902a3e71dcaf18d0e60b95726fc0336
eveuuh <eve.jourdan@gmail.com>
2020-09-21 17:25:24 +0200

    WIP

---
+++ main.js
@@ -0,0 +1,34 @@
+const book = [{ 
+    'title': 'harry potter1',
+    'author': 'jk rowling',
+    'price': 15,
+    'image': '',
+    'resume':'superbe histoire de sorcelerie'
+    }, 
+    {
+    'title': 'harry potter2',
+    'author': 'jk rowling',
+    'price': 17,
+    'image': '',
+    'resume':'superbe histoire de sorcelerie'
+    }, 
+    {
+    'title': 'harry potter3',
+    'author': 'jk rowling',
+    'price': 17,
+    'image': '',
+    'resume':'superbe histoire de sorcelerie' 
+    },
+    {
+    'title': 'harry potter4',
+    'author': 'jk rowling',
+    'price': 17,
+    'image': '',
+    'resume':'superbe histoire de sorcelerie'
+    },
+];
+
+let recommandation = []
+
+function afficheRecommandation()
+    console.log(book.value);
