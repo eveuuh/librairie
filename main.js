d0378d97f8b25a20dec0015aad63eb55982e19e0
eveuuh <eve.jourdan@gmail.com>
2020-09-22 11:28:11 +0200

    size expected and js added

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const book = [{ 
-    'title': 'harry potter1',
+
+/*const form = document.getElementById('searchbar');*/
+
+/* on creer un tableau associatif de clés valeurs*/
+const books = [{ 
+    'title': 'harry potter à l\'ecole des sorciers',
     'author': 'jk rowling',
     'price': 15,
-    'image': '',
+    'image': "assets/hp1.jpg", 
     'resume':'superbe histoire de sorcelerie'
     }, 
     {
@@ -15,20 +19,37 @@ const book = [{
     {
     'title': 'harry potter3',
     'author': 'jk rowling',
-    'price': 17,
+    'price': 22,
     'image': '',
     'resume':'superbe histoire de sorcelerie' 
     },
     {
     'title': 'harry potter4',
     'author': 'jk rowling',
-    'price': 17,
+    'price': 20,
     'image': '',
     'resume':'superbe histoire de sorcelerie'
     },
 ];
 
-let recommandation = []
 
-function afficheRecommandation()
-    console.log(book.value);
+/* Afficher 3 livres au hasard*/
+
+function afficheRecommandation(){ 
+    console.log(books[Math.floor(Math.random()*4)]);
+};
+for ( let pas = 0; pas < 3; pas++) {
+    afficheRecommandation();
+};
+
+/* afficher les livres inferieurs a 20€ */
+
+function afficheLivres(prixMax){
+   const result = books.filter(books => books.price < prixMax);
+   console.log(result);
+
+};
+
+afficheLivres(18);
+
+    
